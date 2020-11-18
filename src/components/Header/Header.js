6968e43d95268a401cb364f8ca817da785e528ed
Dorota Janus <dot@MacBook-Pro-Dorota.local>
2020-11-18 15:36:07 +0100

    React config

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+
+import React from "react";
+import "./header.scss"
+
+export const Header = () => {
+    return (
+        <header className="App-header">
+            <h1>Hej</h1>
+        </header>
+
+    )
+
+
+}
+
+
