4f2056488e1a13a5de892e5dfe4d525d67bcffb6
Dorota Janus <dot@MacBook-Pro-Dorota.local>
2020-11-18 18:03:34 +0100

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,41 @@
-
 import React from "react";
 import "./header.scss"
 
 export const Header = () => {
     return (
-        <header className="App-header">
-            <h1>Hej</h1>
+        <header className="header">
+            <div className="header-img">
+                <img className="header-img-one" src={"../../images/Home-Hero-Image.jpg"} alt="oddam w dobre rece"/>
+            </div>
+            <div className="header-content">
+                <nav className="header-nav">
+                    <ul>
+                        <li>
+                            Start
+                        </li>
+                        <li>
+                            O co chodzi?
+                        </li>
+
+                        <li>
+                            O nas
+                        </li>
+
+                        <li>
+                            Fundacja i organizacje
+                        </li>
+
+                        <li>
+                            Kontakt
+                        </li>
+                    </ul>
+                </nav>
+                <div className="header-title">
+                    <h1>Zacznij pomagać! <br/> Oddaj niechciane rzeczy w zaufane ręce</h1>
+                </div>
+            </div>
+
+
         </header>
 
     )
