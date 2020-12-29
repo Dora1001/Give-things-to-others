25cf09c1257b7f9e766e19caa866a70b1ddf7a2b
Dorota Janus <dot@MacBook-Pro-Dorota.local>
2020-12-29 19:49:07 +0100

    Better css on homepage

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -6,18 +6,18 @@ import "./counter.scss"
 export const Counter = () => {
     return (
 
-        <div className="counter-cointainer">
-            <div className="counter-cointainer-box">
+        <div className="counter-container">
+            <div className="counter-container-box">
                 <h2>10</h2>
                 <h3>ODDANYCH WORKÓW</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
             </div>
-            <div className="counter-cointainer-box">
+            <div className="counter-container-box">
                 <h2>5</h2>
                 <h3>WSPARTYCH ORGANIZACJI</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
             </div>
-            <div className="counter-cointainer-box">
+            <div className="counter-container-box">
                 <h2>7</h2>
                 <h3>ZORGANIZOWANYCH ZBIÓREK</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
